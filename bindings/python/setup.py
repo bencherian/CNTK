3af0b2ccd6a5d9786662fa846dc813238ddd28e9
bencherian <benjamin.cherian@live.com>
2017-02-24 13:19:33 -0800

    Use AVX2 in release and change wheel version

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -174,8 +174,7 @@ if IS_PY2:
     cntk_install_requires.append('enum34>=1.1.6')
 
 setup(name="cntk",
-      version="2.0.beta12.0",
-      url="http://cntk.ai",
+      version="2.0.beta12+.bc-master"
       ext_modules=[cntk_module],
       packages=packages,
       install_requires=cntk_install_requires,
