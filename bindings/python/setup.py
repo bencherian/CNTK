4de0625426120e9254eb632838aed1c8f7dfcbef
bencherian <benjamin.cherian@live.com>
2017-04-21 13:19:11 -0700

    Enable AVX2 and new libs and change Python version

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -175,8 +175,7 @@ if IS_PY2:
     cntk_install_requires.append('enum34>=1.1.6')
 
 setup(name="cntk",
-      version="2.0rc2",
-      url="http://cntk.ai",
+      version="2.0rc2+bc",
       ext_modules=[cntk_module],
       packages=packages,
       install_requires=cntk_install_requires,
